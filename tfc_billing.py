4dfed65cd5879b312c5db51afe4b43e6c306d384
tushar9861 <tushar9861@users.noreply.github.com>
2026-07-26 04:14:48 +0530

    Rename Entry Journal to Procurement and bump to v1.2.5

--- tfc_billing.py
+++ tfc_billing.py
@@ -24,7 +24,7 @@ import re as re_module
 import requests
 import subprocess
 
-APP_VERSION = "1.2.4"
+APP_VERSION = "1.2.5"
 import math
 import random
 import os
@@ -7794,7 +7794,7 @@ class MainWindow(QMainWindow):
         
         action_user_manual = QAction("User Manual", self)
         action_user_manual.triggered.connect(self.open_user_manual)
-        action_procurement = QAction("🧾 Entry Journal", self)
+        action_procurement = QAction("🧾 Procurement", self)
         action_procurement.triggered.connect(self.open_procurement_dialog)
         
         # QR Code Action
